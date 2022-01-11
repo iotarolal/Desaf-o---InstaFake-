aabd31d9a59d15b7763fcfa1b1f145ce95a6eccc
Ivan Otarola <iotarolal@gmail.com>
2022-01-11 10:38:27 -0300

    crea botones de siguientes imagenes y logout

--- public/desafio-instafake/assets/scripts.js
+++ public/desafio-instafake/assets/scripts.js
@@ -1,6 +1,6 @@
 // Inicio
 let token;
-
+let nropaginas = 1;
 $('#formphotos').on('submit', async function (ev) {
   // evitemos que se recargue la pagina
     ev.preventDefault();
@@ -10,51 +10,104 @@ $('#formphotos').on('submit', async function (ev) {
 
     // ahora vamos a solicitar el Token
     const data = await fetch('/api/login', {
-    method: 'POST',
-    body: JSON.stringify({ email, password })
+        method: 'POST',
+        body: JSON.stringify({ email, password })
     })
     const jwt = await data.json()
 
     // guardamos el token en una variable
     token = jwt.token
 
-    console.log(token)
+    localStorage.setItem ("token",token);
+
     getphotos(token)
 })
 
+
+
+//(function() {
+//    If (localStorage.getItem("token")) {
+//        const token = localStorage.SetItem("token", token)
+//        getPhotos(token)
+//    }
+//})
+
 async function getphotos(token) {
-    // Ahora vamos a buscar los Fotos
-
-
-    const data2 = await fetch('/api/photos', {
+// Ahora vamos a buscar los Fotos
+    const data2 = await fetch('/api/photos?limit=10', {
         headers: {
             Authorization: `Bearer ${token}`
         }
-        })
-        const respuesta = await data2.json()
-        const photos = respuesta.data
-    
-        llenarfotos(photos)
+    })
+
+    const respuesta = await data2.json()
+    const photos = respuesta.data
+
+    $("#formphotos").addClass("d-none").removeClass("d-block");
+    $("#photos").addClass("d-block").removeClass("d-none");
+            
+    $("#salir").removeClass("d-none").addClass("d-block");
+    $("#mas").removeClass("d-none").addClass("d-block");
+
+console.log(photos)
+    llenarfotos(photos)
 }
 
 
 function llenarfotos(photos) {
   //  llenamos las card con las fotos 
+
     for (let photo of photos) {
 
-        console.log(photo.url)
-        $('#fotos').append(`
-            <div class=card col-4 offset-4">
-                <img src="${photo.download_url}" class=”card-img-top" alt=”fotos">
+        $('#photos').append(`
+            <div class="card" style="width: 08rem;">
+                <img src="${photo.download_url}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <p class="card-text">Autor: ${photo.author} <p>
+                    <h5 class="card-title">Autor: ${photo.author}</h5>
                 </div>
             </div>
         `)
+
     }
 
     // Escondemos el formulario
     $('#div-form').removeClass('d-block').addClass('d-none')
-    // Mostramos la tabla
-    $('#div-tabla').removeClass('d-none').addClass('d-block')
+    // Mostramos photos
+    $("#photos").removeClass("d-none").removeClass("d-block");
+
 }
+
+// logout del usuario
+$('#salir').on('click', async function (ev) {
+    // evitemos que se recargue la pagina
+        ev.preventDefault();
+        // obtenemos el email y el password
+        $("#formphotos").addClass("d-block").removeClass("d-none");
+        $("#photos").removeClass("d-block").addClass("d-none");
+        $("#salir").removeClass("d-block").addClass("d-none");
+        $("#salir").removeClass("d-block").addClass("d-none");
+    
+})
+
+// Mostrar mas fotografias
+$('#mas').on('click', async function (ev) {
+    // evitemos que se recargue la pagina
+        ev.preventDefault();
+        // obtenemos el email y el password
+    
+        const data2 = await fetch('/api/photos?page=2', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        
+        const respuesta = await data2.json();
+        const photos = respuesta.data;
+
+        console.log("mas maspaginas");
+        nropaginas ++;
+        llenarfotos(photos);
+
+})
+
+// Ahora vamos a buscar los Fotos
